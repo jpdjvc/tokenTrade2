5d842f2d98184b3a04c288941abf1b8a3ce165ac
"John Paul Dillard" <"johnpauldillard1@gmail.com">
2022-05-19 15:39:35 -0700

    Updated makeOffer() and storage for offers

--- contracts/Seller.sol
+++ contracts/Seller.sol
@@ -11,7 +11,18 @@ contract Seller is Ownable {
         uint tokenId;
     }
 
+    struct Offer {
+        //TODO:: reorder for storage optimization
+        uint[] listingIds;
+        address[] nftAddresses;
+        uint[] tokenIds;
+        address[] erc20Addresses;
+        uint[] erc20Amounts;
+        uint weiValue;
+    }
+
     mapping (uint => Listing) private _idToListing;
+    Offer[] private _offers;
 
     constructor(address owner_) {
         _transferOwnership(owner_);
@@ -23,18 +34,17 @@ contract Seller is Ownable {
         _listingIdCounter++;
     }
 
-    function makeOffer( uint[] listingIds_, 
-                        address[] nftAddresses_, 
-                        uint[] tokenIds_,
-                        address[] erc20Addresses_, 
-                        uint[] erc20Amounts_, uint weiValue) 
+    function makeOffer( uint[] calldata listingIds_, 
+                        address[] calldata nftAddresses_, 
+                        uint[] calldata tokenIds_,
+                        address[] calldata erc20Addresses_, 
+                        uint[] calldata erc20Amounts_, uint weiValue) 
                         public {
         require(nftAddresses_.length == tokenIds_.length, "not same length");
-
         // offer validation here
+
+
+        _offers.push(Offer(listingIds_, nftAddresses_, tokenIds_, erc20Addresses_, erc20Amounts_, weiValue));
     }
-
-
-
 
 }
