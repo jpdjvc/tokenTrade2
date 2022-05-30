ad15223d7ce4f2dd7116c98459594860430639f8
"John Paul Dillard" <"johnpauldillard1@gmail.com">
2022-05-29 22:26:17 -0700

    added _validate() function and acceptOffer()

--- contracts/Seller.sol
+++ contracts/Seller.sol
@@ -25,17 +25,17 @@ contract Seller is Ownable {
     struct Listing {
         address nftAddress;
         uint tokenId;
+        bool isValidListing;
     }
 
     struct Offer {
         //TODO:: reorder for storage optimization
-        address offerer;
+        address offererAddress;
         uint[] listingIds;
-        address nftAddress;
+        address[] nftAddresses;
         uint[] tokenIds;
         address[] erc20Addresses;
         uint[] erc20Amounts;
-        uint weiValue;
     }
 
     mapping (uint => Listing) private _idToListing;
@@ -46,39 +46,73 @@ contract Seller is Ownable {
     }
 
     function createListing(address nftAddress_, uint tokenId_) public onlyOwner {
-        Listing memory l = Listing(nftAddress_, tokenId_);
+        Listing memory l = Listing(nftAddress_, tokenId_, true);
         _idToListing[_listingIdCounter] = l;
         _listingIdCounter++;
     }
 
     function makeOffer( uint[] calldata listingIds_, 
-                        address nftAddress_, 
+                        address[] calldata nftAddresses_, 
                         uint[] calldata tokenIds_,
                         address[] calldata erc20Addresses_, 
-                        uint[] calldata erc20Amounts_, uint weiValue) 
+                        uint[] calldata erc20Amounts_) 
                         public {
 
-        require(erc20Addresses_.length == erc20Amounts_.length, "not same length");
-        // offer validation here
-        address sender = msg.sender;
-
-        // require that Seller contract is approved to transfer all erc20 tokens
-        for(uint i=0; i<erc20Addresses_.length; i++){
-            ERC20 erc20 = ERC20(erc20Addresses_[i]);
-            require(erc20.allowance(sender, address(this)) >= erc20Amounts_[i]);
-        }
-
-        //require that Seller contract is approved to transfer ERC721 tokens
-        ERC721 erc721 = ERC721(nftAddress_);
-        for(uint i=0; i<tokenIds_.length; i++){
-            require(erc721.getApproved(tokenIds_[i])==address(this));
-        }
-        _offers.push(Offer(msg.sender, listingIds_, nftAddress_, tokenIds_, erc20Addresses_, erc20Amounts_, weiValue));
+        require(erc20Addresses_.length == erc20Amounts_.length, "erc not same length");
+        require(nftAddresses_.length == tokenIds_.length, "tokens not same length");
+        
+        // offer validation here... should this be done here? -- Expensive?
+        _offers.push(Offer(msg.sender, listingIds_, nftAddresses_, tokenIds_, erc20Addresses_, erc20Amounts_));
     }
 
 
-    // function acceptOffer(uint offerId_) public onlyOwner{
-    //     Offer memory toAccept = _offers[offerId_];
-    // }
+    function acceptOffer(uint offerId_) public onlyOwner{
+        _validateOffer(offerId_);
+        Offer memory offer = _offers[offerId_];
+
+        // transfer offerers ERC20 tokens
+        for(uint i=0; i<offer.erc20Addresses.length; i++){
+            require(ERC20(offer.erc20Addresses[i]).transferFrom(offer.offererAddress, msg.sender, offer.erc20Amounts[i]));
+        }
+
+        // transfer Owners NFTs
+        for(uint i=0; i<offer.listingIds.length; i++){
+            Listing memory l = _idToListing[offer.listingIds[i]];
+            ERC721(l.nftAddress).safeTransferFrom(msg.sender, offer.offererAddress, l.tokenId);
+        }
+
+        // transfer offerers NFTs
+        for(uint i=0; i<offer.tokenIds.length; i++){
+            ERC721(offer.nftAddresses[i]).safeTransferFrom(offer.offererAddress, msg.sender, offer.tokenIds[i]);
+        }
+
+
+
+
+    }
+
+    function _validateOffer(uint offerId_) internal view {
+        require(offerId_>=0 && offerId_ < _offers.length);
+        Offer memory offer = _offers[offerId_];
+        
+        // validate listing is still valid
+        for(uint i=0; i<offer.listingIds.length; i++){
+            Listing memory l = _idToListing[offer.listingIds[i]];
+            require(l.isValidListing);
+            require(ERC721(l.nftAddress).getApproved(l.tokenId) == address(this));
+        }
+
+
+        // require that Seller contract is approved to transfer all erc20 tokens
+        for(uint i=0; i<offer.erc20Addresses.length; i++){
+            require(ERC20(offer.erc20Addresses[i]).allowance(offer.offererAddress, address(this)) >= offer.erc20Amounts[i]);
+        }
+
+        //require that Seller contract is approved to transfer ERC721 tokens
+        for(uint i=0; i<offer.tokenIds.length; i++){
+            require(ERC721(offer.nftAddresses[i]).getApproved(offer.tokenIds[i])==address(this));
+        }
+    }
+
 
 }
