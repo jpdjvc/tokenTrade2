7bc78db41d9d0d7ecbd790f08e1b9c640fd3befc
jpdjvc <johnpauldillard1@gmail.com>
2022-06-06 21:41:38 -0700

    w/ standard encoding

--- test/broker-test.js
+++ test/broker-test.js
@@ -48,20 +48,28 @@ uint[] offerTokenAmounts
 // }
 
 
+function encode(data){
+    let dataTypes = ["address[]", "uint[]", "address[]", "uint[]", "address[]", "uint[]"];
+    let encodedData = ethers.utils.defaultAbiCoder.encode(dataTypes, data);
+    return encodedData;
+}
 
 
 async function createSignature(data, signer){
-    if(data.length != 9){
+    if(data.length != 6){
         console.log("Data incomplete in createSignature()");
         return -1;
     }
 
-    let dataTypes = ["uint256", "uint256", "uint256", "address[]", "uint[]", "address[]", "uint[]", "address[]", "uint[]"];
-    let encodedData = ethers.utils.solidityPack(dataTypes, data);
-    let messageHash = ethers.utils.solidityKeccak256(dataTypes, data);
+    let dataTypes = ["address[]", "uint256[]", "address[]", "uint256[]", "address[]", "uint256[]"];
+    // let encodedData = ethers.utils.defaultAbiCoder.encode(dataTypes, data);
+    let encodedData = encode(data);
+    let messageHash = ethers.utils.keccak256(encodedData);
     let sig = await signer.signMessage(ethers.utils.arrayify(messageHash));
     return [sig, encodedData];
 }
+
+
 
 
 
@@ -118,6 +126,7 @@ describe('Basic Broker Testing', async function() {
 
     it("Makes listing", async function(){
         this.listings = [];
+        await this.nfts[0].connect(this.accounts[0]).approve(this.broker.address, 0);
         this.listings.push(([this.nfts[0].address, 0]));
     });
 
@@ -137,9 +146,6 @@ describe('Basic Broker Testing', async function() {
         // create offer by account[1] to bid on single nft from account[0]
         this.offers = [];
         let data = [            
-            1,
-            1,
-            1,
             [this.listings[0][0]],
             [this.listings[0][1]],
             [this.nfts[0].address],
@@ -148,45 +154,65 @@ describe('Basic Broker Testing', async function() {
             [10]
         ];
 
+        
+        await this.erc20s[0].connect(this.accounts[1]).approve(this.broker.address, 10);
+        await this.nfts[0].connect(this.accounts[1]).approve(this.broker.address, 1);
+
         const [signedMessage, dataBytes] = await createSignature(data, this.accounts[1])
         
+
         this.offers.push({
             "signature":signedMessage,
-            "offererAddress": this.accounts[0].address,
+            "offererAddress": this.accounts[1].address,
             "data": data,
-            "dataBytes": dataBytes
+            "dataBytes": dataBytes,
+            // 'packedData': packedData
         });
         // console.log(this.offers[0]);
     });
 
     it("verify Offerer signature", async function(){
         expect(await this.broker.connect(this.accounts[0])._verifySignature(
-            this.offers[0]["signature"], 
-            this.offers[0]["dataBytes"],
-            this.accounts[1].address
+            this.offers[0].signature, 
+            this.offers[0].dataBytes,
+            this.offers[0].offererAddress
             )).to.equal(true);
     });
 
     it("Verifies seller signature", async function (){
-        const [signedMessage, dataBytes] = await createSignature(this.offers[0].data, this.accounts[0])
-
+        [this.signedMessage, _] = await createSignature(this.offers[0].data, this.accounts[0])
         expect(await this.broker.connect(this.accounts[0])._verifySignature(
-            signedMessage,
+            this.signedMessage,
             this.offers[0].dataBytes,
             this.accounts[0].address
         )).to.equal(true, "did not verify seller signature correctly");
     });
 
     it("accepts offer", async function(){
-        const [signedMessage, dataBytes] = await createSignature(this.offers[0].data, this.accounts[0])
+        // const [signedMessage, dataBytes] = await createSignature(this.offers[0].data, this.accounts[0])
         // console.log(this.offers[0]);
+        // console.log(
+        //     this.signedMessage, 
+        //     this.offers[0].signature,
+        //     this.offers[0].offererAddress,
+        //     this.offers[0].dataBytes);
+        // console.log("offer: ", this.offers[0]);
+
         await this.broker.connect(this.accounts[0]).executeTrade(
-            signedMessage,
+            this.signedMessage,
             this.offers[0].signature,
             this.offers[0].offererAddress,
-            this.offers[0].data
+            this.offers[0].dataBytes,
+            {gasLimit: 30000000}
         )
-    })
+    });
+
+    // it("tests decoding", async function(){
+    //     console.log("Address of listing NFT: ", this.nfts[0].address);
+    //     let encodedData = encode(this.offers[0].data);
+    //     await this.broker.connect(this.accounts[0]).decodeData(encodedData);
+
+    // })
     
 
     
